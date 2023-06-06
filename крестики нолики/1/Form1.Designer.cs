95a3b09e0055f31bbe0bcfd591b3fffa71047c3e
miloyezzlo <135716335+miloyezzlo@users.noreply.github.com>
2023-06-06 19:05:10 +0500

    Add files via upload

---
+++ крестики нолики/1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(612, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 39);
+            this.label1.TabIndex = 9;
+            this.label1.Text = ".";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button10.Location = new System.Drawing.Point(612, 116);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(205, 70);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "НАЧАТЬ";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "крестики нолики";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ крестики нолики/1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        private int x = 12, y = 12;
+        private Button[,] buttons = new Button[3, 3];
+        private int p;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Height = 700;
+            this.Width = 900;
+            this.MaximizeBox= false;
+            p = 1;
+            label1.Text = "ход 1 игрока";
+            for (int i = 0; i < buttons.Length / 3; i++)
+            {
+                for (int j = 0; j < buttons.Length / 3; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(143, 139);
+                }
+            }
+            setButtons();
+        }
+        private void setButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Location = new Point(12 + 149 * j, 12 + 145 * i);
+                    buttons[i, j].Click += button1_Click;
+                    buttons[i, j].Font = new Font(new FontFamily("Microsoft Sans Serif"), 80);
+                    buttons[i, j].Text = "";
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch(p)
+            {
+                case 1: 
+                    sender.GetType().GetProperty("Text").SetValue(sender, "x");
+                    p = 0;
+                    label1.Text = "ход 2 игрока";
+                    break;  
+                case 0:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "o");
+                    p = 1;
+                    label1.Text = "ход 1 игрока";
+                    break;
+            }
+            sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            checkWin();
+        }
+        private void checkWin()
+        {
+            if (buttons[0, 0].Text == buttons[0, 1].Text && buttons[0, 1].Text == buttons[0, 2].Text)
+            {
+
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("Вы победили!");
+                    return;
+                }
+            }
+            if (buttons[1, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[1, 2].Text)
+            {
+                if (buttons[1, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[2, 0].Text == buttons[2, 1].Text && buttons[2, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 0].Text == buttons[1, 0].Text && buttons[1, 0].Text == buttons[2, 0].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 1].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 1].Text)
+            {
+                if (buttons[0, 1].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 2].Text == buttons[1, 2].Text && buttons[1, 2].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 2].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[2, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[0, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            //else if((buttons[0, 0].Text != buttons[0, 1].Text && buttons[0, 1].Text != buttons[0, 2].Text)&& (buttons[1, 0].Text != buttons[1, 1].Text && buttons[1, 1].Text != buttons[1, 2].Text)&& (buttons[2, 0].Text != buttons[2, 1].Text && buttons[2, 1].Text != buttons[2, 2].Text)&& (buttons[0, 0].Text != buttons[1, 0].Text && buttons[1, 0].Text != buttons[2, 0].Text)&& (buttons[0, 1].Text != buttons[1, 1].Text && buttons[1, 1].Text != buttons[2, 1].Text)&& (buttons[0, 2].Text != buttons[1, 2].Text && buttons[1, 2].Text != buttons[2, 2].Text)&& (buttons[0, 0].Text != buttons[1, 1].Text && buttons[1, 1].Text != buttons[2, 2].Text)&& (buttons[2, 0].Text != buttons[1, 1].Text && buttons[1, 1].Text != buttons[0, 2].Text))
+            //{
+               // if((buttons[1, 0].Text != "")&& (buttons[2, 0].Text != "") && (buttons[0, 0].Text != "")&& (buttons[0, 1].Text != "")&& (buttons[0, 2].Text != "")&& (buttons[0, 0].Text != "")&& (buttons[2, 0].Text != ""))
+               // MessageBox.Show("Ничья!");
+           // }
+            
+                
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+    }
+}
